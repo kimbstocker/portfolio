2c8b5e52d84a31634fd39b465e5e5a810763bc09
Kim Stocker <kimbstocker@gmail.com>
2021-11-08 13:01:22 +1100

    add js function to display dynamic name

---
+++ src/script/common.js
@@ -0,0 +1,30 @@
+
+// <script>
+        
+      
+
+// function display() {
+//     DispWin = window.open('', 'NewWin', 'toolbar=no,status=no,width=300,height=200')
+//     message = "<ul><li><b>NAME: </b>" + document.form1.yourname.value;
+//     message += "<li><b>ADDRESS: </b>" + document.form1.address.value;
+//     message += "<li><b>PHONE: </b>" + document.form1.phone.value + "</ul>";
+//     DispWin.document.write(message);
+// }
+
+// </script>
+
+
+
+// {/* <script LANGUAGE="JavaScript" type="text/javascript" src="../script/common.js">
+        
+      
+
+//         function display() {
+//             DispWin = window.open('', 'NewWin', 'toolbar=no,status=no,width=300,height=200')
+//             message = "<ul><li><b>NAME: </b>" + document.form1.yourname.value;
+//             message += "<li><b>ADDRESS: </b>" + document.form1.address.value;
+//             message += "<li><b>PHONE: </b>" + document.form1.phone.value + "</ul>";
+//             DispWin.document.write(message);
+//         }
+        
+//         </script> */}
