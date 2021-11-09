8e8f0a3a5a625825da4fdcf9538e7c0ccbd48c55
Kim Stocker <kimbstocker@gmail.com>
2021-11-09 16:01:26 +1100

    show dynamic name on contact page elements

--- src/script/common.js
+++ src/script/common.js
@@ -2,20 +2,34 @@
 
 function returnname() {
     var x = document.getElementById("name-input");
-    var welcome = "Hello ";
+    var name = "";
     var i;
     for (i = 0; i < x.length; i++) {
-        welcome += x.elements[i].value;
+        name += x.elements[i].value;
     }
-    document.getElementById("personal-greeting").innerHTML = welcome + "!!!";
+    document.getElementById("personal-greeting").innerText = "Hello " + name + "!!!";
     document.getElementById("welcome1").style.display = 'none';
     document.getElementById("welcome2").style.display = 'flex';
+    localStorage.setItem("welcome-name", name);
 }
 
 function passName() {
-    document.getElementById("personal-greeting").innerHTML = "Hi I'll pass! :P";
+    document.getElementById("personal-greeting").innerText = "Hi I'll pass! :P";
     document.getElementById("welcome1").style.display = 'none';
     document.getElementById("welcome2").style.display = 'flex';
 
 }
 
+function thankname() {
+    var name = localStorage.getItem("welcome-name");
+    if (name && name.length > 0) {
+        document.getElementById("thank-name").innerText = "Thank you, " + name + "!!!";
+    }
+}
+
+function prefillName() {
+    var name = localStorage.getItem("welcome-name");
+    if (name && name.length > 0) {
+        document.getElementById("contact-name").value = name;
+    }
+}
