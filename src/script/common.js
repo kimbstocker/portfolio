ce55c7a569a11a4e8d53706ea20fea3063bcf726
Kim Stocker <kimbstocker@gmail.com>
2021-11-13 23:15:32 +1100

    tidy JS

--- src/script/common.js
+++ src/script/common.js
@@ -1,81 +1,69 @@
 
-function returnname() {
-    var x = document.getElementById("name-input");
-    var name = "";
-    var i;
-    for (i = 0; i < x.length; i++) {
-        name += x.elements[i].value;
-    }
+function greetName() {
+    var name = document.getElementById("visitor-name").value;
     document.getElementById("personal-greeting").innerText = "Hello " + name + "!!!";
     document.getElementById("welcome1").style.display = 'none';
     document.getElementById("welcome2").style.display = 'flex';
     localStorage.setItem("welcome-name", name);
 }
 
-function passName() {
+function greetNamePass() {
     document.getElementById("personal-greeting").innerText = "Hi I'll Pass! 😜";
     document.getElementById("welcome1").style.display = 'none';
     document.getElementById("welcome2").style.display = 'flex';
-
-}
-
-function thankname() {
-    var name = localStorage.getItem("welcome-name");
-    if (name && name.length > 0) {
-        document.getElementById("thank-name").innerText = "Thank you, " + name + "!!!";
-    }
 }
 
 function prefillName() {
     var name = localStorage.getItem("welcome-name");
     if (name && name.length > 0) {
+        document.getElementById("thank-name").innerText = "Thank you, " + name + "!!!";
         document.getElementById("contact-name").value = name;
     }
 }
 
-function ThisDog() {
+function clickedDog() {
     document.getElementById("cat").style.display = 'none';
     document.getElementById("dog").style.display = 'none';
     document.getElementById("be-friend").style.display = 'block';
 }
 
-function ThatCat() {
+function clickedCat() {
     document.getElementById("cat").style.display = 'none';
     document.getElementById("dog").style.display = 'none';
     document.getElementById("co-exist").style.display = 'block';
 }
 
-function ThisTea() {
+function clickedTea() {
     document.getElementById("tea").style.display = 'none';
     document.getElementById("coffee").style.display = 'none';
     document.getElementById("hightea").style.display = 'block';
 }
 
-function ThatCoffee() {
+function clickedCoffee() {
     document.getElementById("tea").style.display = 'none';
     document.getElementById("coffee").style.display = 'none';
     document.getElementById("soylatte").style.display = 'block';
 }
 
-function ThisNetflix() {
+function clickedNetflix() {
     document.getElementById("netflix").style.display = 'none';
     document.getElementById("youtube").style.display = 'none';
     document.getElementById("binge").style.display = 'block';
 }
 
-function ThatYoutube() {
+function clickedYouTube() {
     document.getElementById("netflix").style.display = 'none';
     document.getElementById("youtube").style.display = 'none';
     document.getElementById("recipe").style.display = 'block';
 }
 
-function ThisIOS() {
+function clickedIOS() {
     document.getElementById("ios").style.display = 'none';
     document.getElementById("android").style.display = 'none';
     document.getElementById("converted").style.display = 'block';
 }
 
-function ThatAndroid() {
+function clickedAndroid() {
     document.getElementById("ios").style.display = 'none';
     document.getElementById("android").style.display = 'none';
     document.getElementById("respect").style.display = 'block';
